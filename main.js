b52e4b33835211613d0f79c5e369651e29983851
rable <73579842+th-rable@users.noreply.github.com>
2024-12-16 11:25:01 +0000

    asdf

--- main.js
+++ main.js
@@ -10,8 +10,22 @@ function login(){
         id: id.value,
         psword: psword.value,
     };
+    console.log(JSON.stringify(req))
+    fetch("https://fastapi-ex-b.onrender.com/auth/login/",{
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    })
+    .then(res=>{
+        if(res.status == 200){
+            alert("로그인 성공");
+        }else return res.json();
+    })
+    .then((res) => {
+        alert(res.detail);
+    })
 
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET','https://google.com');
-    xhr.send();
+    
 }
